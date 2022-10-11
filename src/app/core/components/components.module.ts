d00ba30b4e6921ad8658a96cc188d98c3872c51d
VitorLima1998 <vitor.xlr8@gmail.com>
2022-10-11 16:18:00 -0400

    project in progress

--- src/app/core/components/components.module.ts
+++ src/app/core/components/components.module.ts
@@ -5,12 +5,16 @@ import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
 import { Card1Component } from './card1/card1.component';
 import { Card2Component } from './card2/card2.component';
+import { Card7Component } from './card7/card7.component';
+import { Card11Component } from './card11/card11.component';
 
 const COMPONENTS = [
   MenuComponent,
   HeaderComponent,
   Card1Component,
   Card2Component,
+  Card7Component,
+  Card11Component,
 ];
 
 @NgModule({

484d439586fca6aefab3cb5d512c2247011656bb
VitorLima1998 <vitor.xlr8@gmail.com>
2022-10-17 16:09:04 -0400

    project in progress

--- src/app/core/components/card5/card5.component.ts
+++ src/app/core/components/card5/card5.component.ts
@@ -1,15 +1,39 @@
 import { Component, OnInit } from '@angular/core';
+import { Chart } from 'node_modules/chart.js';
 
 @Component({
   selector: 'app-card5',
   templateUrl: './card5.component.html',
-  styleUrls: ['./card5.component.css']
+  styleUrls: ['./card5.component.css'],
 })
 export class Card5Component implements OnInit {
-
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
+    const myChart = new Chart('Chart', {
+      type: 'bar',
+      data: {
+        labels: ['Purple', 'Orange'],
+        datasets: [
+          {
+            label: '# of Votes',
+            data: [12, 19, 3, 5, 2, 3],
+            backgroundColor: [
+              'rgba(153, 102, 255, 0.2)',
+              'rgba(255, 159, 64, 0.2)',
+            ],
+            borderColor: ['rgba(153, 102, 255, 1)', 'rgba(255, 159, 64, 1)'],
+            borderWidth: 1,
+          },
+        ],
+      },
+      options: {
+        scales: {
+          y: {
+            beginAtZero: true,
+          },
+        },
+      },
+    });
   }
-
 }
